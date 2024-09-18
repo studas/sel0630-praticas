17cea5b019f25b49ffad29185af555e4cf59d284
Matheus Cirillo <matheuscirillo@usp.br>
2024-09-18 14:57:36 -0300

    new dht library

--- pratica2/parte2/dht_basic.py
+++ pratica2/parte2/dht_basic.py
@@ -1,27 +1,21 @@
+import RPi.GPIO as GPIO
+import dht11
 import time
-import board
-import adafruit_dht
 
-# Sensor data pin is connected to GPIO 4
-sensor = adafruit_dht.DHT22(board.D4)
-# Uncomment for DHT11
-#sensor = adafruit_dht.DHT11(board.D4)
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+GPIO.cleanup()
+
+# read data using pin 14
+instance = dht11.DHT11(pin = 14)
+result = instance.read()
 
 while True:
-    try:
-        # Print the values to the serial port
-        temperature_c = sensor.temperature
-        temperature_f = temperature_c * (9 / 5) + 32
-        humidity = sensor.humidity
-        print("Temp={0:0.1f}ºC, Temp={1:0.1f}ºF, Humidity={2:0.1f}%".format(temperature_c, temperature_f, humidity))
+    if result.is_valid():
+        print("Temperature: %-3.1f C" % result.temperature)
+        print("Humidity: %-3.1f %%" % result.humidity)
+    else:
+        print("Error: %d" % result.error_code)
+    time.sleep(2)
 
-    except RuntimeError as error:
-        # Errors happen fairly often, DHT's are hard to read, just keep going
-        print(error.args[0])
-        time.sleep(2.0)
-        continue
-    except Exception as error:
-        sensor.exit()
-        raise error
-
-    time.sleep(3.0)
+# initialize GPIO
