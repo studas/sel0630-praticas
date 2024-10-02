40666ca3f12a299a2b8a77588dc9b4d6833e544e
Matheus Cirillo <matheuscirillo@usp.br>
2024-10-02 15:04:53 -0300

    fixing loop

--- pratica3/parte2/text.py
+++ pratica3/parte2/text.py
@@ -8,8 +8,7 @@ GPIO.setwarnings(False)
 leitor = SimpleMFRC522()
 print("Aproxime a tag do leitor para leitura.")
 while True: #loop
-#cria as variáveis "id" e "texto", e as atribui as leituras da id e do texto coletado
-da tag pelo leitor, respectivamente
-id,texto = leitor.read()
-print("ID: {}\nTexto: {}".format(id, texto)) #exibe as informações coletadas
-sleep(3) #aguarda 3 segundos para nova leitura
+#cria as variáveis "id" e "texto", e as atribui as leituras da id e do texto coletado da tag pelo leitor, respectivamente
+    id,texto = leitor.read()
+    print("ID: {}\nTexto: {}".format(id, texto)) #exibe as informações coletadas
+    sleep(1) #aguarda 3 segundos para nova leitura
